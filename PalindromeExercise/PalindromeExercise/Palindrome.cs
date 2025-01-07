aeaa8a262a28319626ce20d61366e71638daa802
Robert Anderson <rganderson0798@gmail.com>
2025-01-06 21:27:49 -0800

    PalindromeExcercise

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PalindromeExercise;
+
+public class Palindrome
+{
+    public bool IsPalindrome(string word)
+    {
+        var reversed = "";
+
+        for (int i =word.Length - 1; i >= 0; i--)
+        {
+            reversed += word[i];
+        }
+
+        if (reversed.ToLower() == word.ToLower())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void Test1(string word, bool expected)
         {
+            var tester = new Palindrome();
+            
+            bool actual = tester.IsPalindrome(word);
+            
+            Assert.Equal(expected, actual);
 
         }
     }
